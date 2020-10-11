a77c51ea9fefc8b9caedc44a7000513cd517af33
unknown <sorina.florean@SVROLP01322.ro.softvision.global>
2020-10-11 23:31:02 +0300

    homework1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        boolean a = false;
+        char b = 'e';
+        int c = 7;
+        long d = 12345678L;
+        double e = 123.43555;
+        //Non primitive: String, Integer;
+
+        // System.out.println("Boolean :" + a); //sugestie sa modific a cu false
+        System.out.print("Character :" + b);
     }
 }
