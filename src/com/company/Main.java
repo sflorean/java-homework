275d006830a40cb6d652a3fe3c16aef35e8e7802
sflorean <sorina.florean@softvision.ro>
2020-10-12 21:02:54 +0300

    Fixed homework 1 - ex 7 and 8

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        //Exercitiul 5
+        //5
         boolean z = false;
         char w = 'e';
         int c = 7;
@@ -17,7 +17,7 @@ public class Main {
         System.out.println("Long :" + d);
         System.out.println("Double :" + e);
 
-        //Exercitiul 6
+        //6
         int x = 3;
         int y = 6;
         System.out.println(++x);
@@ -25,41 +25,24 @@ public class Main {
         System.out.println(x - y);
         System.out.println(x == y);
 
-        //Exercitiul 7a
+        //7
         int a = 3;
         int b = 6;
         if (b > a) {
             System.out.println(b +"is larger than"+ a);
         } else if (a > b ){
             System.out.println(a + "is larger than"+ b);
-        } else if (b == b){
-            System.out.println(b +"is equal to"+ b);
+        } else {
+            System.out.println(a +"is equal to"+ b);
         }
 
-        //Exercitiul 7b
-        if (b < a) {
-            System.out.println(a +"is larger than"+ b);
-        } else {
-            System.out.println(b +"is larger than"+ a);
-        }
-        if (b > a) {
-            System.out.println(b +"is larger than"+ a);
-        } else {
-            System.out.println(a +"is larger than"+ b);
-        }
-        if (b == b) {
-            System.out.println(b +"is equal to"+ b);
-        } else {
-            System.out.println("This will never be true!");
-        }
 
-        //Exercitiul 8
-        int[] intArray = new int[]{1, 2, 3, 4, 55, 6, 77, 8, 99, 10};
-        System.out.print("Even Numbers are: ");
-        for (int a8 = 0; a8 <= 10; a8++) {
-            if (a8 % 2 == 0) {
-                System.out.print(a8 + " ");
-            }
+        //8
+        int [] n = new int [10];
+        for (int i=0; i<n.length; i++) {
+            n[i] = (int)(Math.floor(Math.random()*100 + 1));
+            if (n[i] % 2 == 0)
+                System.out.println("Even numbers are " + n[i]);
         }
 
     }
